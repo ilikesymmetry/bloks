caede0cf1942b5e208aafe3efac1456cd5e3038a
Conner Swenberg <cls364@cornell.edu>
2024-06-21 00:39:20 -0400

    Add landing page

--- site/vocs.config.ts
+++ site/vocs.config.ts
@@ -1,15 +1,66 @@
-import { defineConfig } from 'vocs'
+import { defineConfig } from "vocs";
 
 export default defineConfig({
-  title: 'Docs',
-  sidebar: [
+  title: "Bloks",
+  baseUrl: "https://bloks.sh",
+  titleTemplate: "%s · Bloks",
+  description:
+    "Build onchain products faster with a modular Solidity framework.",
+  iconUrl: { light: "/favicons/light.png", dark: "/favicons/dark.png" },
+  logoUrl: { light: "/icon-light.png", dark: "/icon-dark.png" },
+  theme: {
+    accentColor: {
+      light: "#121212",
+      dark: "#fafafa",
+    },
+    variables: {
+      color: {
+        background: {
+          light: "#EDECEC",
+          dark: "#222222",
+        },
+        border: {
+          light: "#EEEEEE",
+          dark: "#333333",
+        },
+      },
+    },
+  },
+  topNav: [{ text: "Docs", link: "/guides/quickstart", match: "/docs" }],
+  socials: [
     {
-      text: 'Getting Started',
-      link: '/getting-started',
+      icon: "github",
+      link: "https://github.com/ilikesymmetry/bloks",
     },
     {
-      text: 'Example',
-      link: '/example',
+      icon: "x",
+      link: "https://x.com/ilikesymmetry",
     },
   ],
-})
+  sidebar: [
+    {
+      text: "Introduction",
+      collapsed: false,
+      items: [
+        {
+          text: "Why Blocks",
+          link: "/introduction/why-bloks",
+        },
+        {
+          text: "Architecture",
+          link: "/introduction/architecture",
+        },
+      ],
+    },
+    {
+      text: "Guides",
+      collapsed: false,
+      items: [
+        {
+          text: "Quickstart",
+          link: "/guides/quickstart",
+        },
+      ],
+    },
+  ],
+});
